ca27b9c222594989aeb82ee1885dc8cb8d8eb2d5
Pepe Roldán <jmroldangil@gmail.com>
2014-01-04 10:19:14 +0100

    La clase trazas ya está preparada y completa. También los cambios hechos en la parte de la topología. No se si subirlo por si hay que merge y cosas así.

--- trazas.cc
+++ trazas.cc
@@ -5,13 +5,24 @@
   del envío de paquetes.
 */
 
-#include "ns3/network-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
 #include "trazas.h"
+
+using namespace ns3;
+
 NS_LOG_COMPONENT_DEFINE ("Trazas");
 
 
 Trazas::Trazas()
-  :  envios_finalizados(0)
+  :  envia_0(0),
+     recibe_0(0),
+     envia_1(0),
+     recibe_1(0)
 {
 }
 
@@ -20,57 +31,65 @@ Trazas::~Trazas()
 }
 
 
-//Métodos para el nodo dorsal de la izquierda L
+//Métodos para el nodo dorsal de la izquierda 0
 //Método llamado en la traza de recepción que aumenta un contador
 void
-Trazas::NodoLRecibe(Ptr<const Packet> paquete)
+Trazas::Router0Recibe(Ptr<const Packet> paquete)
 {
-  recibe_L++;
+  NS_LOG_FUNCTION(this);
+  recibe_0++;
 }
-void
 //Método que devuelve el contador de paquetes recibidos
 int
-Trazas::GetNodoLRecibe()
+Trazas::GetRouter0Recibe()
 {
-  return recibe_L;
+  return recibe_0;
 }
 //Método llamado en la traza de envio que aumenta un contador
 void
-Trazas::NodoLEnvia(Ptr<const Packet> paquete)
+Trazas::Router0Envia(Ptr<const Packet> paquete)
 {
-  envia_L++;
+  envia_0++;
 }
 //Método que devuelve el contador de paquetes enviados
 int
-Trazas::GetNodoLEnvia()
+Trazas::GetRouter0Envia()
 {
-  return envia_L;
+  return envia_0;
 }
-//Métodos para el nodo dorsal de la derecha R
+//Métodos para el nodo dorsal de la derecha 1
 //Métodos llamado en la traza de recepción que aumenta un contador
 void
-Trazas::NodoRRecibe(Ptr<const Packet> paquete)
+Trazas::Router1Recibe(Ptr<const Packet> paquete)
 {
-  recibe_R++;
+  recibe_1++;
 }
 //Método que devuelve el contador de paquetes recibidos. 
 int
-Trazas::GetNodoRRecibe()
+Trazas::GetRouter1Recibe()
 {
-  return recibe_R;
+  return recibe_1;
 }
 //Métodos llamado en la traza de envio que aumenta un contador
 void
-Trazas::NodoREnvia(Ptr<const Packet> paquete)
+Trazas::Router1Envia(Ptr<const Packet> paquete)
 {
-  envia_R++;
+  envia_1++;
 }
 //Método que devuelve el contador de envío.
 int
-Trazas::GetNodoREnvia()
+Trazas::GetRouter1Envia()
 {
-  return envia_R;
+  return envia_1;
 }
-
+void
+Trazas::ImprimeTrazas()
+{
+  std::cout<<"Trazas obtenidas: "<<std::endl;
+  std::cout<<"Paquetes enviados por el router 0: "<<envia_0<<std::endl;
+  std::cout<<"Paquetes recibidos por el router 0: "<<recibe_0<<std::endl;
+  std::cout<<"Paquetes enviados por el router 1: "<<envia_1<<std::endl;
+  std::cout<<"Paquetes recibidos por el router 1: "<<recibe_1<<std::endl;
+}
 
 #endif
--- trazas.h
+++ trazas.h
@@ -1,8 +1,24 @@
-# if 0
+#if 0
 /*
-  Trazas.h
-  librería de la clase Trazas
+  Fichero: trazas.h
+  Librería de la clase Trazas
 */
+using namespace ns3;
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <cassert>
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/callback.h"
+#include "ns3/internet-module.h"
+
+
 class Trazas
 {
 public:
@@ -11,18 +27,32 @@ public:
 
   virtual ~Trazas ();
 
-  void 
+  void Router0Recibe (Ptr<const Packet> paquete);
 
+  int GetRouter0Recibe ();
 
+  void Router0Envia (Ptr<const Packet> paquete);
+
+  int GetRouter0Envia ();
+
+  void Router1Recibe (Ptr<const Packet> paquete);
+
+  int GetRouter1Recibe ();
+
+  void Router1Envia (Ptr<const Packet> paquete);
+
+  int GetRouter1Envia ();
+  
+  void ImprimeTrazas ();
 
 
 private:
   //Variable que cuenta los paquetes cuyo envío ha finalizado.
-  int envia_L;
-  int recibe_L;
-  int envia_R;
-  int envia_L;
+  int envia_0;
+  int recibe_0;
+  int envia_1;
+  int recibe_1;
 
 };
-#endif
 
+#endif
