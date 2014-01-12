8c696afdd25759ac3bd6f011ac2549d683dbb368
Carlos Rodriguez <crresse@gmail.com>
2014-01-12 13:28:41 +0100

    He comentado el archivo topologia.cc

--- principal.cc
+++ principal.cc
@@ -23,7 +23,6 @@ main ( int argc, char * argv[])
   LogComponentEnable("Trazas", LOG_LEVEL_INFO);
   LogComponentEnable("Topologia", LOG_LEVEL_INFO);
   LogComponentEnable("Principal", LOG_LEVEL_INFO);
-  // LogComponentEnable("Trazas", LOG_LEVEL_ALL);
 
   Config::SetDefault ("ns3::CsmaNetDevice::EncapsulationMode", StringValue ("Dix"));
 
